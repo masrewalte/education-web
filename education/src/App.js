d994dd5e878c5d405e4021595e008a15d7e7c58f
masrewalte <140162608+masrewalte@users.noreply.github.com>
2023-12-22 21:39:47 +0300

    HEADER

---
+++ education/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/common/heading/Header";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
